677a1899570ef9e20e2f24d3ddc9c46731d26a05
tnishida <tnishida0309@gmail.com>
2019-11-07 21:53:35 +0900

    no21

--- dai3syou/No21.py
+++ dai3syou/No21.py
@@ -1,9 +1,9 @@
 from dai3syou import No20
 
-lines = No20.extract_from_json(u"イギリス").split("\n")
+lines = No20.extract_from_json("イギリス").split("\n")
 
 for line in lines:
     if "Category" in line:
         print(line)
 
-#print([line for line in lines if "Category" in line])
+print([line for line in lines if "Category" in line])
