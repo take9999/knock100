5c977c9079d92469e842a9ece30e171965f60b1b
tnishida <tnishida0309@gmail.com>
2019-11-13 20:54:44 +0900

    no25 update

--- dai3syou/No25_28.py
+++ dai3syou/No25_28.py
@@ -1,18 +1,20 @@
 from dai3syou import No20
 import re
 
+
 def remove_markup(str):
-    #強調
+    # 強調
     str = re.sub(r"'{2,5}", r"", str)
-    #内部リンク
+    # 内部リンク
     str = re.sub(r"\[{2}([^|\]]+?\|)*(.+?)\]{2}", r"\2", str)
-    #言語を指定した表記
+    # 言語を指定した表記
     str = re.sub(r"\{{2}.+?\|.+?\|(.+?)\}{2}", r"\1 ", str)
-    #コメント
+    # コメント
     str = re.sub(r"<.*?>", r"", str)
-    #外部リンク
+    # 外部リンク
     str = re.sub(r"\[.*?\]", r"", str)
     return str
+
 
 temp_dict = {}
 lines = No20.extract_from_json(u"イギリス").split("\n")
